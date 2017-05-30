eaea265d6d8753ea43eb2e00bf994223beb9f97e
Julien Sobczak <julien.sobczak@gmail.com>
2017-05-30 23:11:07 +0200

    Generate epubs from 2014 to May 2015

---
+++ history.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python main.py --start 2014-10-08 --end 2015-01-01 --filename TheMorningPaper2014
+python main.py --start 2015-01-01 --end 2015-07-01 --filename TheMorningPaper2015-Part-I
+python main.py --start 2015-07-01 --end 2016-01-01 --filename TheMorningPaper2015-Part-II
+python main.py --start 2016-01-01 --end 2016-07-01 --filename TheMorningPaper2016-Part-I
+python main.py --start 2016-07-01 --end 2017-01-01 --filename TheMorningPaper2016-Part-II
+python main.py --start 2017-01-01 --end 2017-05-01 --filename TheMorningPaper2017-Part-I
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+$ python main.py --start 2014-10-08 --end 2016-01-01
+"""
+
 import pypub
 import re, unicodedata
 import requests
@@ -8,18 +12,11 @@ from bs4 import BeautifulSoup
 import time
 import os
 import argparse
+import collections
 
-
-
-def slugify(value):
-    """
-    Converts to lowercase, removes non-word characters (alphanumerics and
-    underscores) and converts spaces to hyphens. Also strips leading and
-    trailing whitespace.
-    """
-    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
-    value = re.sub('[^\w\s-]', '', value).strip().lower()
-    return re.sub('[-\s]+', '-', value)
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 
@@ -28,11 +25,18 @@ def generate_epub(name, start=None, end=None):
     Main method.
     """
 
-    epub = pypub.Epub('The Morning Paper')
+    # Collect post to sort them after
+    posts = {}
 
-    # We test with a downloaded page to avoid excessive connections
-    with open("resources/page_15.html") as f:
-        html_doc = f.read()
+    page = 1
+    while True:
+        r = requests.get('https://blog.acolyer.org/page/%s/' % page)
+        if r.status_code == 404:
+            print("Reach the end of the blog at page %s" % page)
+            break
+        page = page + 1
+
+        html_doc = r.text
         soup = BeautifulSoup(html_doc, 'html.parser')
         for post in soup.select(".post"):
             #print(post)
@@ -44,27 +48,49 @@ def generate_epub(name, start=None, end=None):
             # Note: the link is useless because the list page contains the full post text.
             href = post_link.attrs['href']
 
-            # Collect and process the title
+            # Collect the title
             title = post_link.get_text()
-            slug_title = slugify(title)
-            short_slug_title = slug_title  # Will be use to create a folder for each post containing pictures
-            if len(short_slug_title) > 20:
-                short_slug_title = short_slug_title[:short_slug_title.index("-", 15)]
+
+            if not title:
+                print("Fail to find the title: %s" % post)
 
             # Collect and parse the data
             date_text = post_date.get_text()  # Ex: December 16, 2016
             conv = time.strptime(date_text, "%B %d, %Y")
             date_en = time.strftime("%Y-%m-%d", conv) # Ex: 2016-12-16
 
+            # Filter according the dates
+            if start and date_en < start:
+                continue
+            if end and date_en >= end:
+                continue
+
+            print("Processing post %s (%s)" % (title, date_en))
+
             # Collect the content
             content = post.select(".entry")[0]
-            content_text = str(content)
+            content_text = u"""
+            <h2>%s</h2>
+            <h3>%s</h3>
+            %s
+            """ % (title, date_text, str(content))
 
-            c = pypub.create_chapter_from_string(content_text, title=title)
-            epub.add_chapter(c)
+            # Post are traversed in reverse order
+            posts[date_en] = {
+                "date": date_text,
+                "title": title,
+                "content": content_text
+            }
 
+    # Sort the post starting from the oldest
+    ordered_posts = collections.OrderedDict(sorted(posts.items()))
+
+    # Generate the target file
+    epub = pypub.Epub(name)
+    for date_en, post in ordered_posts.iteritems():
+        c = pypub.create_chapter_from_string(post["content"], title=post["title"])
+        epub.add_chapter(c)
     epub.create_epub(os.getcwd())
-
 
 
 if __name__ == "__main__":
@@ -74,8 +100,8 @@ if __name__ == "__main__":
                         help='filter posts published after the start date')
     parser.add_argument('--end', dest='end', default=None,
                         help='filter posts published before the end date')
-    parser.add_argument('--filename', dest='filename', default="The-Morning-Paper.epub",
-                        help='name of the output file')
+    parser.add_argument('--filename', dest='filename', default="The-Morning-Paper",
+                        help='name of the output file without the extension')
     args = parser.parse_args()
 
     generate_epub(args.filename, start=args.start, end=args.end)
